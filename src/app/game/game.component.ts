b15cdefa2ec95be2110c295cf089f023c19dc59f
Manu Kapolke <manukapolke@gmail.com>
2023-08-31 15:02:11 +0200

    game class

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
+  game: Game = new Game();
   cardIsPicked: boolean = false;
+
+  ngOnInit() {
+    this.initGame();
+  }
+
+  initGame() {
+    // this.game = new Game();
+    console.log(this.game);
+  }
 
   takeCard() {
     this.cardIsPicked = true;
